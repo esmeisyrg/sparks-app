87867407e0d4cede87b833a46b756ace8d057526
meyricks <esmeisytarina@gmail.com>
2025-06-05 19:07:01 -0400

    add: more organization

--- Sparks/Sparks/Models/Transaction.swift
+++ Sparks/Sparks/Models/Transaction.swift
@@ -1,6 +1,7 @@
+// Datos de ejemplo
+
 import SwiftUI
 
-// Modelo de transacción
 struct Transaction: Identifiable {
     let id = UUID()
     let name: String
@@ -9,12 +10,17 @@ struct Transaction: Identifiable {
     let isPositive: Bool
     let image: String
 }
-
-// Datos de ejemplo
 let sampleTransactions: [Transaction] = [
-    Transaction(name: "Supermercado Bravo", description: "Compra", amount: "-2,350", isPositive: false, image: "cart"),
-    Transaction(name: "Luis Rodríguez", description: "Transferencia", amount: "+12,500", isPositive: true, image: "person"),
-    Transaction(name: "Altice", description: "Pago de servicio", amount: "-1,200", isPositive: false, image: "wifi"),
-    Transaction(name: "Netflix", description: "Suscripción", amount: "-550", isPositive: false, image: "film"),
-   // Transaction(name: "YouTube Premium", description: "Suscripción", amount: "-850", isPositive: false, image: "speaker.wave.2.fill")
+    Transaction(
+        name: "Supermercado Bravo", description: "Compra", amount: "-2,350", isPositive: false,
+        image: "cart"),
+    Transaction(
+        name: "Luis Rodríguez", description: "Transferencia", amount: "+12,500", isPositive: true,
+        image: "person"),
+    Transaction(
+        name: "Altice", description: "Pago de servicio", amount: "-1,200", isPositive: false,
+        image: "wifi"),
+    Transaction(
+        name: "Netflix", description: "Suscripción", amount: "-550", isPositive: false,
+        image: "film"),
 ]
